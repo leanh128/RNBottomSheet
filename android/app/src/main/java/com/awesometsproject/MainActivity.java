549a57ad56f75c0a14e279247d5c4822b4330d3a
lee <anh.le.tuan@ntq-solution.com.vn>
2021-12-06 10:22:46 +0700

    - Init

---
+++ android/app/src/main/java/com/awesometsproject/MainActivity.java
@@ -0,0 +1,66 @@
+package com.awesometsproject;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactFragment;
+import com.facebook.react.ReactRootView;
+import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
+
+public class MainActivity extends AppCompatActivity implements DefaultHardwareBackBtnHandler {
+//
+//    /**
+//     * Returns the name of the main component registered from JavaScript. This is used to schedule
+//     * rendering of the component.
+//     */
+//    @Override
+//    protected String getMainComponentName() {
+//        return "AwesomeTSProject";
+//    }
+//
+//    @Override
+//    protected ReactActivityDelegate createReactActivityDelegate() {
+//        return new ReactActivityDelegate(this, getMainComponentName()) {
+//            @Override
+//            protected ReactRootView createRootView() {
+//                LayoutInflater inflater = LayoutInflater.from(MainActivity.this);
+//                ViewGroup vg = (ViewGroup) inflater.inflate(R.layout.activity_main, null);
+//                return vg.findViewById(R.id.container);
+//            }
+//        };
+//    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Fragment reactNativeFragment = new ReactFragment.Builder()
+                .setComponentName("AwesomeTSProject")
+//                .setLaunchOptions(getLaunchOptions("test message"))
+                .build();
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, reactNativeFragment,"").commitAllowingStateLoss();
+    }
+
+    @Override
+    public void invokeDefaultOnBackPressed() {
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+}
